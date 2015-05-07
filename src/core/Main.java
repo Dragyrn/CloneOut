0c7fd4111a9ac833e7a36f9d0dfc8221ba3b5f1f
Dragyrn <jeobiii@gmail.com>
2015-05-07 12:16:25 -0400

    Added assorted proposal documents and settings

---
+++ src/core/Main.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * CloneOut - A Breakout Clone - VBISD Final Project
+ * Copyright (C) 2015 Dragyrn
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any 
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+	public void start(Stage primaryStage)
+	{
+		{ //License check
+			
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
